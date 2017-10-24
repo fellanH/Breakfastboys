5483631bc9437dbb06f8a53417f14c38205e6b1a
fellanH <fehellstrom@gmail.com>
2017-10-24 20:46:08 +0200

    language picker

--- js/all.js
+++ js/all.js
@@ -1134,3 +1134,77 @@ function split_height_init(){
             
     })(jQuery);
 }
+
+/* ---------------------------------------------
+ Multi language
+ --------------------------------------------- */
+    var languageSelect = document.getElementById('LanguageSelect');
+    var selectedLanguage = readCookie('language');
+    
+    if ((selectedLanguage == 'english') || (selectedLanguage == 'swedish')) {
+    $(".language select").val(selectedLanguage);
+    var sel = $(".language select").val(selectedLanguage);
+    if (selectedLanguage == 'english') {
+            $(".english-block").css("display", "block");
+            $(".swedish-block").css("display", "none");
+            $(".english-inline-block").css("display", "inline-block");
+            $(".swedish-inline-block").css("display", "none");  
+            
+        }else{
+            $(".english-block").css("display", "none");
+            $(".swedish-block").css("display", "block");
+            $(".english-inline-block").css("display", "none");
+            $(".swedish-inline-block").css("display", "inline-block");
+            
+        }
+    }
+
+    $(".language select").bind('change', function() {
+        //on change set cookie and...
+        setCookie('language', this.value, 365);
+        
+        //change css depending on what is selected
+        var sel = $(".language select").val();
+        if (sel=='english') {
+            $(".english-block").css("display", "block");
+            $(".swedish-block").css("display", "none");
+            $(".english-inline-block").css("display", "inline-block");
+            $(".swedish-inline-block").css("display", "none");
+        }else {
+            $(".english-block").css("display", "none");
+            $(".swedish-block").css("display", "block");
+            $(".english-inline-block").css("display", "none");
+            $(".swedish-inline-block").css("display", "inline-block");
+           
+        }
+        
+
+    });
+
+    function saveLanguage(cookieValue)
+    {
+    var sel = document.getElementById('LanguageSelect');
+    setCookie('language', cookieValue, 365);
+    }
+
+    function setCookie(cookieName, cookieValue, nDays) {
+    var today = new Date();
+    var expire = new Date();
+
+    if (nDays==null || nDays==0)
+        nDays=1;
+
+    expire.setTime(today.getTime() + 3600000*24*nDays);
+    document.cookie = cookieName+"="+escape(cookieValue) + ";expires="+expire.toGMTString();
+    }
+
+    function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+    }
